d49847c0e9685d507682dfd224311ce146d9e3d1
Abhishek Singh <abhisheksingh4115@gmail.com>
2025-10-06 16:14:00 +0530

    Add Sim Product

---
+++ DemoUI/Controllers/SIMController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoUI.Controllers
+{
+    public class SIMController : Controller
+    {
+        public IActionResult SIMProducts()
+        {
+            return View();
+        }
+    }
+}

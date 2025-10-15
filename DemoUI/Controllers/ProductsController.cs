53e9e504a13a0d5991620d1cba56c7736c1a815c
Ashwani Shukla <ashwanishuklatest@gmail.com>
2025-10-15 17:17:43 +0530

    Product Search

---
+++ DemoUI/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using DemoUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Json;
+
+public class ProductsController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public ProductsController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [HttpGet]
+    public IActionResult ProductsSearch()
+    {
+
+        return View();
+    }
+}

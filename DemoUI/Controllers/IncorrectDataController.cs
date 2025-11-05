4a0b29cdce0e6c295b1837639a536d6c908c64aa
Ashwani Shukla <ashwanishuklatest@gmail.com>
2025-11-05 18:00:00 +0530

    Visual Category Checker

---
+++ DemoUI/Controllers/IncorrectDataController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoUI.Controllers
+{
+    public class IncorrectDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult VisualCategoryChecker()
+        {
+            return View();
+        }
+
+        public IActionResult MissingProduct()
+        {
+            return View();
+        }
+    }
+}

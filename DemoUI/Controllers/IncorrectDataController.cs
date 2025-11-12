56d35892bb36d398f59664acea63f97800db02b9
Ashwani Shukla <ashwanishuklatest@gmail.com>
2025-11-12 12:52:40 +0530

    Ui CHanges

--- DemoUI/Controllers/IncorrectDataController.cs
+++ DemoUI/Controllers/IncorrectDataController.cs
@@ -17,5 +17,7 @@ namespace DemoUI.Controllers
         {
             return View();
         }
+
+   
     }
 }
---
+++ DemoUI/Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoUI.Controllers
+{
+    [Route("upload")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost("product-image")]
+        public async Task<IActionResult> UploadProductImage(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("No file uploaded.");
+
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads/products");
+            if (!Directory.Exists(uploadsFolder))
+                Directory.CreateDirectory(uploadsFolder);
+
+            var uniqueFileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var relativePath = $"/uploads/products/{uniqueFileName}";
+
+            return Ok(new { path = relativePath });
+        }
+    }
+
+}
